6e177f2af966df6f678a591b6dce4aae1f803d5c
malvinharis <alvinharis08@gmail.com>
2022-02-10 15:52:02 +0700

    Add Voucher Message

--- components/voucher/VoucherMessage.vue
+++ components/voucher/VoucherMessage.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- Voucher Message -->
-  <v-card class="voucher-message__card" color="white">
+  <v-card class="voucher-message__card" color="light-green  darken-1">
     <!-- Voucher Message Head -->
     <div class="voucher-message__head">
       <v-btn
-        color="black"
+        color="white"
         icon
         @click="
           (event) => {
@@ -27,26 +27,36 @@
               src="https://dummyimage.com/600x400/e3e3e3/fff&text=+"
             ></v-img>
           </div>
-          <div class="voucher-message__voucher-title">Home Living</div>
-          <div class="voucher-message__voucher-subtitle">1 Item</div>
+          <div class="voucher-message__voucher-group">
+            <div class="voucher-message__voucher-title">Home Living</div>
+            <div class="voucher-message__voucher-subtitle">1 Item</div>
+          </div>
         </div>
       </div>
       <!-- Display Date & Status -->
       <div class="voucher-message__summary-group">
-        <div class="voucher-message__date">
-          <v-icon>mdi-date</v-icon>
-          <div class="voucher-message__date-content">24 Jan 2022 - 10:00</div>
+        <div class="voucher-message__item">
+          <v-icon>mdi-calendar-month-outline</v-icon>
+          <div class="voucher-message__item-content">24 Jan 2022 - 10:00</div>
         </div>
-        <div class="voucher-message__status">
-          <v-icon>mdi-date</v-icon>
-          <div class="voucher-message__date-content">On Process</div>
+        <div class="voucher-message__item">
+          <v-icon>mdi-file-document-outline</v-icon>
+          <div
+            class="voucher-message__item-content blue--text font-weight-medium"
+          >
+            On Process
+          </div>
         </div>
       </div>
       <!-- Display Total Payment -->
       <div class="voucher-message__summary-group">
         <div class="voucher-message__payment">
           <div class="voucher-message__payment-title">Total Payment</div>
-          <div class="voucher-message__payment-price">Rp. 3.000.000</div>
+          <div
+            class="voucher-message__payment-price orange--text text--accent-4"
+          >
+            Rp. 3.000.000
+          </div>
         </div>
       </div>
       <!-- Display Transaction -->
@@ -58,9 +68,14 @@
               TADA-7791273
             </div>
           </div>
-          <div class="voucher-message__transaction-button">
-            <v-btn color="black" icon> Copy </v-btn>
-          </div>
+          <v-btn
+            class="voucher-message__transaction-button"
+            outlined
+            color="orange accent-4"
+            small
+          >
+            Copy
+          </v-btn>
         </div>
       </div>
       <!-- Display Payment Detail -->
@@ -68,11 +83,11 @@
         <div class="voucher-message__detail">
           <div class="voucher-message__detail-group">
             <div class="voucher-message__detail-title">Qty</div>
-            <div class="voucher-message__detail-vaue">1</div>
+            <div class="voucher-message__detail-value">1</div>
           </div>
           <div class="voucher-message__detail-group">
             <div class="voucher-message__detail-title">Amount</div>
-            <div class="voucher-message__detail-vaue">Rp. 3.000.000</div>
+            <div class="voucher-message__detail-value">Rp. 3.000.000</div>
           </div>
         </div>
       </div>
@@ -81,11 +96,11 @@
         <div class="voucher-message__detail">
           <div class="voucher-message__detail-group">
             <div class="voucher-message__detail-title">Merchant</div>
-            <div class="voucher-message__detail-vaue">My Brand</div>
+            <div class="voucher-message__detail-value">My Brand</div>
           </div>
           <div class="voucher-message__detail-group">
             <div class="voucher-message__detail-title">Payment Method</div>
-            <div class="voucher-message__detail-vaue">Bank BCA</div>
+            <div class="voucher-message__detail-value">Bank BCA</div>
           </div>
         </div>
       </div>
@@ -93,9 +108,10 @@
     <div class="voucher-message__action">
       <v-btn
         block
-        color="green accent-4"
+        color="white"
         class="voucher-message__action-button white--text"
         large
+        outlined
       >
         Finish
       </v-btn>
